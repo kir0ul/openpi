499025c5c84da046ec4cfa441efc520cfe5d80e9
Andrea Pierré <andrea_pierre@brown.edu>
2026-07-10 01:34:33 -0400

    chore: DL checkpoints in public folder

--- src/openpi/shared/download.py
+++ src/openpi/shared/download.py
@@ -17,7 +17,8 @@ import tqdm_loggable.auto as tqdm
 
 # Environment variable to control cache directory path, ~/.cache/openpi will be used by default.
 _OPENPI_DATA_HOME = "OPENPI_DATA_HOME"
-DEFAULT_CACHE_DIR = "~/.cache/openpi"
+# DEFAULT_CACHE_DIR = "~/.cache/openpi"
+DEFAULT_CACHE_DIR = "/home/public/apierre/.cache/openpi"
 
 logger = logging.getLogger(__name__)
 
